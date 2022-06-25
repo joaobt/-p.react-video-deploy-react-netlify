5e8b645316773d62408f808b35dc8eb932ac287c
Joao Batista <joao4729@gmail.com>
2022-06-25 19:37:20 -0300

    Passando variavel de ambiente

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   fetch(process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1")
+   .then(() => console.log("Deu certo?"))
+   .catch(() => console.log("Deu errado?"))
   return (
     <div className="App">
       <header className="App-header">
